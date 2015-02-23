e69b0822c31fa698f8beec3ae73348310cb7b276
Jerold Lewis <jeroldlewis@Jerolds-MacBook-Air.local>
2015-02-22 21:48:54 -0500

    first project 6 commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+#Gemfile
+
+source 'https://rubygems.org'
+ruby '2.2.0'
+
+gem 'sinatra', '~> 1.4.5'
---
+++ app.rb
@@ -0,0 +1,11 @@
+#app.rb
+
+require 'sinatra'
+require 'sinatra/content_for'
+
+class MyWebApp < Sinatra::Base
+  helpers Sinatra::ContentFor
+  get "/" do
+    erb:"index"
+  end
+end
